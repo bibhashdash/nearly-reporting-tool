23c97f0e2ec9ec342667dab7998b66e9b8b61c1e
bibhashdash <b.dash@12os.co.uk>
2024-05-19 21:35:38 +0100

    fixing linting errors

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,27 +10,27 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      screenOptions={{
+      screenOptions={ {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+      } }>
       <Tabs.Screen
         name="index"
-        options={{
+        options={ {
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon name={ focused ? 'home' : 'home-outline' } color={ color } />
           ),
-        }}
+        } }
       />
       <Tabs.Screen
         name="explore"
-        options={{
+        options={ {
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={ focused ? 'code-slash' : 'code-slash-outline' } color={ color } />
           ),
-        }}
+        } }
       />
     </Tabs>
   );
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,13 @@
 import { Link, Stack } from 'expo-router';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
+      <Stack.Screen options={ { title: 'Oops!' } } />
+      <View style={ styles.container }>
         <Text>This screen doesn't exist.</Text>
-        <Link href="/" style={styles.link}>
+        <Link href="/" style={ styles.link }>
           <Text>Go to home screen!</Text>
         </Link>
       </View>
