72b0164cd566688afc68c8c5897b715166089779
bibhashdash <b.dash@12os.co.uk>
2024-05-20 11:31:43 +0100

    adding tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,13 +4,19 @@ import React from 'react';
 import { TabBarIcon } from 'nearly-components';
 import { Colors } from 'nearly-constants';
 import { useColorScheme } from 'nearly-utilities';
-
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={ {
+        tabBarStyle: {
+          height: 100,
+          paddingBottom: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 16,
+        },
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
       } }>
@@ -24,11 +30,29 @@ export default function TabLayout() {
         } }
       />
       <Tabs.Screen
-        name="explore"
+        name="report"
         options={ {
-          title: 'Explore',
+          title: 'Report',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={ focused ? 'code-slash' : 'code-slash-outline' } color={ color } />
+            <TabBarIcon name={ focused ? 'document' : 'document-outline' } color={ color } />
+          ),
+        } }
+      />
+      <Tabs.Screen
+        name="stats"
+        options={ {
+          title: 'Stats',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={ focused ? 'bar-chart' : 'bar-chart-outline' } color={ color } />
+          ),
+        } }
+      />
+      <Tabs.Screen
+        name="support"
+        options={ {
+          title: 'Support',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={ focused ? 'help-circle' : 'help-circle-outline' } color={ color } />
           ),
         } }
       />
--- app/(tabs)/support.tsx
+++ app/(tabs)/support.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from 'react-native';
 
-export default function TabTwoScreen() {
+export default function TabFourScreen() {
   return (
     <View>
       <Text>
-        Explore
+        Support
       </Text>
     </View>
   );
