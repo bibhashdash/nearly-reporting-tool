71e8232ca056cf681e3b9e027edf28f46d3dcb31
bibhashdash <b.dash@12os.co.uk>
2024-05-20 14:25:49 +0100

    adding appbar headr

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import { Appbar } from 'react-native-paper';
 
 import { useColorScheme } from 'nearly-utilities';
 
@@ -29,7 +31,15 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={ colorScheme === 'dark' ? DarkTheme : DefaultTheme }>
       <Stack>
-        <Stack.Screen name="(tabs)" options={ { headerShown: false } } />
+        <Stack.Screen name="(tabs)" options={
+          {
+            header: () =>
+              <Appbar.Header>
+                <Appbar.Content title="NEARLY" />
+                <Appbar.Action icon="account" onPress={ () => router.push('/profile') } />
+              </Appbar.Header>
+          } }
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
