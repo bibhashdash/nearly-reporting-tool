0a87a2c25439010f521a2b8d66d6717c7f4a3772
bibhashdash <b.dash@12os.co.uk>
2024-06-03 11:44:40 +0100

    setting up image picker

--- app/(tabs)/report.tsx
+++ app/(tabs)/report.tsx
@@ -1,13 +1,36 @@
-import { Pressable, ScrollView, View } from 'react-native';
-import { Button, Icon, Text, TextInput, useTheme } from 'react-native-paper';
+import { Image, Pressable, ScrollView, View } from 'react-native';
+import { Button, IconButton, Text, TextInput, useTheme } from 'react-native-paper';
 import { useState } from 'react';
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function TabTwoScreen() {
   const [title, setTitle] = useState<string>('')
   const [description, setDescription] = useState<string>('');
   const [date, setDate] = useState<Date>(new Date());
+  const [image, setImage] = useState<string | null>(null);
+
   const { colors } = useTheme();
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: false,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const getImageSource = () => {
+    if (image) {
+      console.log(image);
+      return image
+    }
+    return '../../assets/images/add_photo_alternate_24dp_FILL0_wght400_GRAD0_opsz24 (1).png'
+  }
 
   return (
     <ScrollView style={ {
@@ -16,14 +39,26 @@ export default function TabTwoScreen() {
       flex: 1,
     } }
     >
-      <View>
-        <Pressable style={ {
+      <View style={ {
+        width: '100%',
+        height: 200,
+        justifyContent: 'center'
+      } }>
+        <Pressable
+          onPress={ () => pickImage() }
+          style={ {
           alignItems: 'center'
         } }>
-          <Icon size={ 64 } source={ require('../../assets/images/add_photo_alternate_24dp_FILL0_wght400_GRAD0_opsz24 (1).png') } />
-          <Text>
-            Add Photo
-          </Text>
+          <Image width={ 300 } height={ 200 } source={ {
+            uri: getImageSource()
+          } } />
+          {
+            !image && (
+              <Text>
+                Add Photo
+              </Text>
+            )
+          }
         </Pressable>
       </View>
       <View style={ {
@@ -53,7 +88,10 @@ export default function TabTwoScreen() {
           flexDirection: 'row',
           alignItems: 'center'
         } }>
-          <Button
+          <IconButton
+            size={ 36 }
+            mode="outlined"
+            icon="calendar"
             onPress={ () => {
               DateTimePickerAndroid.open({
                 value: date,
@@ -61,17 +99,7 @@ export default function TabTwoScreen() {
                 mode: 'date',
               })
             } }
-            mode="outlined"
-            labelStyle={ {
-              color: colors.tertiary
-            } }
-            style={ {
-            borderColor: colors.tertiary,
-              borderWidth: 2,
-            alignSelf: 'flex-start',
-          } }>
-            Date of Near-Miss
-          </Button>
+          />
           <Text variant="bodyLarge" style={ {
             fontWeight: 'bold'
           } }>
