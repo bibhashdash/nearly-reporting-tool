60e1702784a7af59402de0db11ce48b57eb6a570
bibhashdash <b.dash@12os.co.uk>
2024-06-13 10:29:58 +0100

    role-based UI display

--- app/(auth)/(tabs)/admin.tsx
+++ app/(auth)/(tabs)/admin.tsx
@@ -1,14 +1,27 @@
-import { Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { Text } from 'react-native-paper';
+
 import { useAuthContext } from 'nearly-contexts';
 
 export default function AdminScreen() {
   const { nearlyUser } = useAuthContext();
   return (
-    <View>
-      <Text>Your role is</Text>
-      <Text>
-        { nearlyUser?.role }
-      </Text>
-    </View>
+    <ScrollView style={ {
+      paddingVertical: 24,
+      paddingHorizontal: 12,
+      flex: 1,
+    } }>
+      <View style={ {
+        gap: 24,
+        flex: 1,
+        alignItems: 'center'
+      } }>
+        <Text variant="titleLarge">Hi { nearlyUser?.displayName }!</Text>
+        <Text>Your role is</Text>
+        <Text>
+          { nearlyUser?.role }
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
